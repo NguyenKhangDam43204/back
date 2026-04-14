d745d9a1424980db93f582990f21f5e80b474d5c
DuyMinhED <minhnd2424@ut.edu.vn>
2026-04-14 17:00:17 +0700

    add documentation for user product config review inventor

--- apps/inventory-service/src/inventory/inventory.module.ts
+++ apps/inventory-service/src/inventory/inventory.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
 import { PublishersModule } from '../publishers/publishers.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PublishersModule],
+  imports: [PrismaModule, PublishersModule],
   controllers: [InventoryController],
   providers: [InventoryService],
   exports: [InventoryService],
